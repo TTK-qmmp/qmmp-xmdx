f3595a73a96f99550d13acff44654ccb50507410
Greedysky <greedysky@163.com>
2022-10-09 10:13:25 +0800

    refactor: Use safe cast operator in global module

--- decoderxmdxfactory.cpp
+++ decoderxmdxfactory.cpp
@@ -6,7 +6,7 @@
 
 bool DecoderXMDXFactory::canDecode(QIODevice *input) const
 {
-    QFile *file = static_cast<QFile*>(input);
+    const QFile * const file = qobject_cast<QFile*>(input);
     if(!file)
     {
         return false;
