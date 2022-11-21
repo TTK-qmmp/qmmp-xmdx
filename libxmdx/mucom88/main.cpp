017aa8b16ad4cd94882b411a03e5b30d70b95534
Greedysky <greedysky@163.com>
2022-11-21 13:36:28 +0800

    refactor: Optimize input module

--- libxmdx/mucom88/main.cpp
+++ libxmdx/mucom88/main.cpp
@@ -14,26 +14,26 @@ int main(int argc, char *argv[])
     module.SetRate(44100);
 
     QFile file("/home/greedysky/sampl2.muc");
-    file.open(QFile::ReadOnly);
+    file.open(QIODevice::ReadOnly);
     QByteArray data = file.readAll();
     module.OpenMemory((uchar*)data.constData(), file.size(), file.fileName().toLocal8Bit().constData());
     module.UseFader(true);
-   qDebug() << module.Play() << module.IsEnd() << module.GetLength();
+    qDebug() << module.Play() << module.IsEnd() << module.GetLength();
 
-   int frames = 1024;
-   char bytes[1024 *4];
-   while(!module.IsEnd())
-   {qDebug() << module.GetPosition();
-       module.Mix((short*)bytes, frames);
+    int frames = 1024;
+    char bytes[1024 *4];
+    while(!module.IsEnd())
+    {
+        qDebug() << module.GetPosition();
+        module.Mix((short*)bytes, frames);
 //       for(int i=0; i<1024*4; ++i)
 //       {
 //           qDebug() << (int)bytes[i];
 //       }
-   }
- qDebug() << (module.tag->title).c_str();
+    }
+    qDebug() << (module.tag->title).c_str();
     qDebug() << (module.tag->composer).c_str();
- qDebug() << (module.tag->author).c_str();
-   qDebug() << "end";
-
+    qDebug() << (module.tag->author).c_str();
+    qDebug() << "end";
     return a.exec();
 }
